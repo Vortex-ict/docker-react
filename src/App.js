538b24f0f42f0814184b7cecde59bd93c3570c48
Rene Vrijenhoek <info@vortex-ict.nl>
2020-11-09 12:38:22 +0100

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dit is de code aangepast met de feature branch en merged in master
+          Dit is de originele code
         </p>
         <a
           className="App-link"
