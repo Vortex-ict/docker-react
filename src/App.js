c94d82352d7079498baf03647f01246ad1cac048
Rene Vrijenhoek <info@vortex-ict.nl>
2020-11-10 08:02:00 +0100

    update travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dit is de originele code
+          Dit is de originele code. Met een kleine aanpassing.
         </p>
         <a
           className="App-link"
